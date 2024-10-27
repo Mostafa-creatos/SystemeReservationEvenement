d80c3e33d1e8a6c84c7fdc23e8e1aebf9b7eaa7c
Mostafa Faouzi <mostafafaouzi16@gmail.com>
2024-10-27 21:53:35 +0100

    first commit

---
+++ SystemeReservationEvenement/src/main/java/com/ENSA/SystemeReservationEvenement/SystemeReservationEvenementApplication.java
@@ -0,0 +1,13 @@
+package com.ENSA.SystemeReservationEvenement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeReservationEvenementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeReservationEvenementApplication.class, args);
+	}
+
+}
---
+++ SystemeReservationEvenement/src/test/java/com/ENSA/SystemeReservationEvenement/SystemeReservationEvenementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ENSA.SystemeReservationEvenement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeReservationEvenementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
